2c31d8a137209863c14e81d979a990d1f1b0e059
sophturn <70330883+sophturn@users.noreply.github.com>
2022-03-23 22:17:59 -0600

    Add files via upload

--- src/TriviaGame/src/TriviaGame.java
+++ src/TriviaGame/src/TriviaGame.java
@@ -11,26 +11,25 @@ public class TriviaGame {
 	ArrayList<Question> finalQuestions;
 
 	public static void main(String[] args) throws FileNotFoundException{
-		//Create a file for questions
-		/* 
-		 * This file will hold all the questions. 
-		 * Lines 1-40 will be easy questions
-		 * Lines 41-80 will medium questions
-		 * Lines 81-120 will be hard questions
-		 * Lines 121-130 will be final questions
-		 */
-		File questions = new File("questions");
-		Scanner sc = new Scanner(questions);
-		for(int i=0; i<40;i++) {
-			//easyQuestions<i> = sc.nextLine();
-		}
-		for(int i=40; i<80;i++) {
-			//mediumQuestions<i> = sc.nextLine();
-		}
-		for(int i=80; i<120;i++) {
-			//hardQuestions<i> = sc.nextLine();
-		} for(int i=120; i<130;i++) {
-			//easyQuestions<i> = sc.nextLine();
+		/* Lines 1-40 hold easy questions
+		 * Lines 41-80 hold medium questions
+		 * Lines 81-120 hold hard questions
+		 * Lines 121-130 hold final questions*/
+		File questions = new File("questions.txt");
+		Scanner sc = new Scanner(questions); //Why?
+		System.out.print(sc.next());
+		while(sc.hasNextLine()) {
+			for(int i=1; i<40;i++) {
+				//easyQuestions<i> = sc.nextLine();
+			}
+			for(int i=40; i<80;i++) {
+				//mediumQuestions<i> = sc.nextLine();
+			}
+			for(int i=80; i<120;i++) {
+				//hardQuestions<i> = sc.nextLine();
+			} for(int i=120; i<130;i++) {
+				//easyQuestions<i> = sc.nextLine();
+			}
 		}
 		sc.close();
 		//JFrame
