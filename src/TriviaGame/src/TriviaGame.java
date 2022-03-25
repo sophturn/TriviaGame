f999cf93b3f8683bbd32dc0c04bc168692d02eda
sophturn <70330883+sophturn@users.noreply.github.com>
2022-03-25 14:03:05 -0600

    Add files via upload

--- src/TriviaGame/src/TriviaGame.java
+++ src/TriviaGame/src/TriviaGame.java
@@ -63,6 +63,43 @@ public class TriviaGame {
 				}
 			}
 		}
+		board[0][0].setText("Translate");
+		board[4][0].setText("Translate");
+		board[0][4].setText("Translate");
+		board[5][3].setText("Translate");
+		board[3][5].setText("Translate");
+		board[3][1].setText("Translate");
+		board[1][3].setText("Translate");
+		board[4][4].setText("Translate");
+		board[2][2].setText("Translate");
+		board[1][0].setText("Number");
+		board[5][0].setText("Number");
+		board[0][3].setText("Number");
+		board[5][4].setText("Number");
+		board[2][5].setText("Number");
+		board[3][2].setText("Number");
+		board[2][1].setText("Number");
+		board[1][0].setText("Number");
+		board[1][4].setText("Number");
+		board[4][3].setText("Number");
+		board[2][0].setText("Verb");
+		board[2][4].setText("Verb");
+		board[3][3].setText("Verb");
+		board[1][1].setText("Verb");
+		board[4][2].setText("Verb");
+		board[5][1].setText("Verb");
+		board[5][5].setText("Verb");
+		board[1][5].setText("Verb");
+		board[0][2].setText("Verb");
+		board[2][3].setText("Choice");
+		board[0][1].setText("Choice");
+		board[0][5].setText("Choice");
+		board[3][0].setText("Choice");
+		board[3][4].setText("Choice");
+		board[1][2].setText("Choice");
+		board[4][1].setText("Choice");
+		board[4][5].setText("Choice");
+		board[5][2].setText("Choice");
 
 //		JPanel fin = new JPanel();
 //		fin.setBounds(350, 350, 100, 100);
